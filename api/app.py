4b94a8a69cf8247074041e7b36c87169a17f1259
Renat Abdrakhmanov <rabdrahmanov314@gmail.com>
2022-11-25 16:28:44 +0600

    Implemented color quantization algorithm

--- api/app.py
+++ api/app.py
@@ -1,8 +1,26 @@
+import cv2
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+from ai_daniyar.utils import *
+from PIL import Image
 
 app = FastAPI()
 
-@app.get("/image_segmentation/")
-def segment_image(image: UploadFile=File(...)):
-    res = {"message": "Hello world!"}
-    return res
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+@app.post("/image_segmentation/")
+def image_segmentation(image: UploadFile=File(...)):
+    array = read_image(image.file)
+    image_shape = array.shape
+    array = process_image(array)
+    labels, centers = cluster(array)
+    segmented_image = segment_image(image_shape, labels, centers)
+    success, im = cv2.imencode('.png', segmented_image)
+    headers = {'Content-Disposition': 'attachment; filename="test.tiff"'}
+    return Response(im.tobytes() , headers=headers, media_type='image/tiff')
+
