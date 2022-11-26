eb8c2d40e18390ba442460b118efa65bd4fe5f25
Renat Abdrakhmanov <rabdrahmanov314@gmail.com>
2022-11-27 00:23:04 +0600

    Implemented k means on the tif images

--- api/app.py
+++ api/app.py
@@ -13,27 +13,34 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
-@app.post("/segmentation/image")
-def image_segmentation(image: UploadFile=File(...)):
-    array = read_image(image.file)
-    image_shape = array.shape
-    array = process_image(array)
-    labels, centers = cluster(array)
-    segmented_image = segment_image(image_shape, labels, centers)
-    success, im = cv2.imencode('.png', segmented_image)
-    headers = {'Content-Disposition': 'attachment; filename="test.tiff"'}
-    return Response(im.tobytes() , headers=headers, media_type='image/tiff')
-
 @app.post("/segmentation/matrix")
+def image_segmentation(image: UploadFile=File(...)):
+    save_file(UPLOAD_FILE_PATH, image)
+    filepath = os.path.join(UPLOAD_FILE_PATH, image.filename)
+    data = read_image(filepath)
+    ndvi = es.normalized_diff(data[3], data[0])
+    flat_data = process_image(ndvi)
+    model = get_model("C:/Users/Acer/Desktop/github/aitu_hackday_rid/jupyters/rid_model.pkl")
+    predicted_data = cluster(model, flat_data)
+    matrix = create_matrix(predicted_data, ndvi)
+    return matrix
+    # array = read_image(image.file)
+    # image_shape = array.shape
+    # array = process_image(array)
+    # labels, centers = cluster(array)
+    # segmented_image = segment_image(image_shape, labels, centers)
+    # success, im = cv2.imencode('.png', segmented_image)
+    # headers = {'Content-Disposition': 'inline; filename="test.tiff"'}
+    # return Response(im.tobytes() , headers=headers, media_type='image/tiff')
+
+@app.post("/segmentation/image")
 def segmentation_matrix(image: UploadFile=File(...)):
-    array = read_image(image.file)
-    image_shape = array.shape
-    array = process_image(array)
-    labels, centers = cluster(array)
-    labeled_matrix = label_matrix(image_shape, labels, centers)
-    stream = io.StringIO()
-    labeled_matrix.to_csv(stream, ";", index=False)
-    headers = {'Content-Disposition': 'attachment; filename="test.csv"'}
-    return StreamingResponse(iter([stream.getvalue()]), 
-                             headers=headers, 
-                             media_type='text/csv')
+    save_file(UPLOAD_FILE_PATH, image)
+    filepath = os.path.join(UPLOAD_FILE_PATH, image.filename)
+    data = read_image(filepath)
+    ndvi = es.normalized_diff(data[3], data[0])
+    flat_data = process_image(ndvi)
+    model = get_model("C:/Users/Acer/Desktop/github/aitu_hackday_rid/jupyters/rid_model.pkl")
+    predicted_data = cluster(model, flat_data)
+    matrix = create_matrix(predicted_data, ndvi)
+    return matrix
